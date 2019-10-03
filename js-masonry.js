de0a7ea795509560702e6135afba0293105dfc78
ujw0l <bktujwol@gmail.com>
2019-10-03 11:59:40 +0545

    Div masonry support added

--- js-masonry.js
+++ js-masonry.js
@@ -14,53 +14,65 @@
     constructor(elems,opt){
 
         let masArr =  Array.from(document.querySelectorAll(elems));
-        masArr.map(
-           
-            el=>{
-           
-            this.layoutBrks(el,opt);
-            window.addEventListener('resize',()=> setTimeout(()=>this.layoutBrks(el,opt)),1000);    
+        masArr.map(el=>{
+                        let allBrks    = undefined != opt && undefined != opt.elSelector ? Array.from(el.querySelectorAll(opt.elSelector))  :  Array.from(el.querySelectorAll('*')); 
+                        if(0 < allBrks.length){
+                          let brkPer = undefined != opt && undefined == opt.elWidth  && true === opt.percentWidth ? allBrks[0].offsetWidth/el.offsetWidth: null;    
+                          this.layoutBrks(el,allBrks,brkPer,opt);
+                          window.addEventListener('resize',()=>this.layoutBrks(el,allBrks,brkPer,opt));  
+                        }  
         });
        
     }
 
-  layoutBrks(el,opt){
-
-        let allBrks    = undefined != opt && undefined != opt.elSelector ? Array.from(el.querySelectorAll(opt.elSelector))  :  Array.from(el.querySelectorAll('*')); 
-        let brkWidth    = undefined != opt && undefined != opt.elWidth ? opt.elWidth : allBrks[0].offsetWidth; 
-        let contWidth    = el.offsetWidth;
-        let rawBrkMargin = undefined != opt && undefined != opt.elMargin ?  opt.elMargin : 0;
-        let rawBrkPerRow = (contWidth-rawBrkMargin )/(brkWidth+rawBrkMargin );
-        let brkPerRow = Math.floor(rawBrkPerRow); 
-        let brkMargin = (((rawBrkPerRow - brkPerRow)*brkWidth) + ((rawBrkPerRow+1)*(rawBrkMargin)))/(brkPerRow+1);
-        let availSpots =  Array();
-        let availTop =  Array();
-        for(let z = 0; z<=brkPerRow-1; z++ ){
-            availTop.push(el.offsetTop+rawBrkMargin);
-            availSpots.push([el.offsetTop+rawBrkMargin, el.offsetLeft+(z*brkWidth)+((z+1)*brkMargin)]);     
-        } 
-      
-         allBrks.map((x,i)=>{
-                let brkDim = this.optBrkSize(brkWidth, el.offsetHeight, x.offsetWidth, x.offsetHeight);
+  layoutBrks(el,allBrks,brkPer,opt){
+            let contWidth    = el.offsetWidth;
+            let brkWidth    = undefined != opt && undefined != opt.elWidth ? opt.elWidth :  null != brkPer ?contWidth*brkPer :allBrks[0].offsetWidth ; 
+            let rawBrkMargin = undefined != opt && undefined != opt.elMargin ?  opt.elMargin : 0;
+            let rawBrkPerRow = (contWidth-rawBrkMargin )/(brkWidth+rawBrkMargin );
+            let brkPerRow = Math.floor(rawBrkPerRow); 
+            let brkMargin = (((rawBrkPerRow - brkPerRow)*brkWidth) + ((rawBrkPerRow+1)*(rawBrkMargin)))/(brkPerRow+1);
+            let availSpots =  Array();
+            let availTop =  Array();
+            for(let z = 0; z<=brkPerRow-1; z++ ){
+                availTop.push(el.offsetTop+rawBrkMargin);
+                availSpots.push([el.offsetTop+rawBrkMargin, el.offsetLeft+(z*brkWidth)+((z+1)*brkMargin)]);     
+            } 
+        
+            allBrks.map((x,i)=>{
                 let placeCount =  1;
-                availSpots.map((n,l) =>{
-                    if( availTop[0] === n[0] && 1 === placeCount){
-                        x.style.width = `${brkDim.width}px`;
-                        x.style.height = `${brkDim.height}px`;
-                        x.style.position = 'absolute'; 
-                        x.style.top = `${n[0]}px`;
-                        x.style.left = `${n[1]}px`;
-                        availTop[0] =  n[0]+brkDim.height+brkMargin;
-                        availSpots[l] = [n[0]+brkDim.height+brkMargin, n[1]]
-                        availTop.sort((a, b)=> a-b);
-                        placeCount++;
+                    availSpots.map((n,l) =>{
+                        if( availTop[0] === n[0] && 1 === placeCount){
+                            x.style.width = `${brkWidth}px`;
+                            x.style.position = 'absolute'; 
+                            x.style.top = `${n[0]}px`;
+                            x.style.left = `${n[1]}px`;
+                            placeCount++;
+                        
+                            if(x.nodeName.toLowerCase() === 'img'){
+                                x.style.height = '';
+                                let brkDim = this.optBrkSize(brkWidth, el.offsetHeight, x.offsetWidth, x.offsetHeight);
+                                x.style.height = `${brkDim.height}px`;
+                                availTop[0] =  n[0]+brkDim.height+brkMargin;
+                                availSpots[l] = [n[0]+brkDim.height+brkMargin, n[1]]
+                                availTop.sort((a, b)=> a-b);
+                            }else{
+                                availTop[0] =  n[0]+x.offsetHeight+brkMargin;
+                                availSpots[l] = [n[0]+x.offsetHeight+brkMargin, n[1]]
+                                availTop.sort((a, b)=> a-b);
+                            }   
+                        }
+                    });
+
+                    if(i === allBrks.length-1){
+                        availTop.sort((a, b)=> b-a);
+                        el.style.height =  (availTop[0] - el.offsetTop + rawBrkMargin)+'px';
+                        if(undefined != opt && 'function' == typeof(opt.callback)){
+                            opt.callback(el);
+                        }
                     }
-                }); 
-                if(i === allBrks.length-1){
-                    availTop.sort(function(a, b){return b-a});
-                    el.style.height =  (availTop[0] - el.offsetTop + rawBrkMargin)+'px';
-                }
-            });
+                });
+        
     }
 
     optBrkSize(availWidth, availHeight, actWidth, actHeight) {
