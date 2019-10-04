b4bd099e5cf3cba35a8604d5e7f94960b4d82737
ujw0l <bktujwol@gmail.com>
2019-10-04 19:32:29 +0545

    On callback add bricks

--- js-masonry.js
+++ js-masonry.js
@@ -8,26 +8,24 @@
 
 
  'use strict'
-
  class jsMasonry{
-
     constructor(elems,opt){
 
         let masArr =  Array.from(document.querySelectorAll(elems));
         masArr.map(el=>{
-                        let allBrks    = undefined != opt && undefined != opt.elSelector ? Array.from(el.querySelectorAll(opt.elSelector))  :  Array.from(el.children); 
-                        if(0 < allBrks.length){
-                          let brkPer = undefined != opt && undefined == opt.elWidth  && true === opt.percentWidth ? allBrks[0].offsetWidth/el.offsetWidth: null;    
-                          this.layoutBrks(el,allBrks,brkPer,opt);
-                          window.addEventListener('resize',()=>this.layoutBrks(el,allBrks,brkPer,opt));  
+                        let elFirstChild    = undefined != opt && undefined != opt.elSelector ? el.querySelector(opt.elSelector)  :  el.children[0];
+                        if(undefined  != elFirstChild){
+                          let brkPer = undefined != opt && undefined == opt.elWidth  && true === opt.percentWidth ? elFirstChild.offsetWidth/el.offsetWidth: null;    
+                          this.layoutBrks(el,opt,brkPer);
+                          window.addEventListener('resize',()=>this.layoutBrks(el,opt,brkPer,event));  
                         }  
         });
-       
     }
 
-  layoutBrks(el,allBrks,brkPer,opt){
+  layoutBrks(el,opt,brkPer,resizeEvnt){
+            let allBrks    = undefined != opt && undefined != opt.elSelector ? Array.from(el.querySelectorAll(opt.elSelector))  :  Array.from(el.children); 
             let contWidth    = el.offsetWidth;
-            let brkWidth    = undefined != opt && undefined != opt.elWidth ? opt.elWidth :  null != brkPer ?contWidth*brkPer :allBrks[0].offsetWidth ; 
+            let brkWidth    = undefined != opt && undefined != opt.elWidth ? opt.elWidth :  undefined != brkPer || null != brkPer ?contWidth*brkPer :allBrks[0].offsetWidth ; 
             let rawBrkMargin = undefined != opt && undefined != opt.elMargin ?  opt.elMargin : 0;
             let rawBrkPerRow = (contWidth-rawBrkMargin )/(brkWidth+rawBrkMargin );
             let brkPerRow = Math.floor(rawBrkPerRow); 
@@ -38,7 +36,6 @@
                 availTop.push(el.offsetTop+rawBrkMargin);
                 availSpots.push([el.offsetTop+rawBrkMargin, el.offsetLeft+(z*brkWidth)+((z+1)*brkMargin)]);     
             } 
-        
             allBrks.map((x,i)=>{
                 let placeCount =  1;
                     availSpots.map((n,l) =>{
@@ -48,7 +45,6 @@
                             x.style.top = `${n[0]}px`;
                             x.style.left = `${n[1]}px`;
                             placeCount++;
-                        
                             if(x.nodeName.toLowerCase() === 'img'){
                                 x.style.height = '';
                             let  brkHt = brkWidth/ x.offsetWidth * x.offsetHeight;
@@ -67,11 +63,14 @@
                     if(i === allBrks.length-1){
                         availTop.sort((a, b)=> b-a);
                         el.style.height =  (availTop[0] - el.offsetTop + rawBrkMargin)+'px';
-                        if(undefined != opt && 'function' == typeof(opt.callback)){
+                        if(undefined != opt && 'function' == typeof(opt.callback) && undefined == resizeEvnt){
                             opt.callback(el);
                         }
+                        if(undefined == brkPer){
+                            window.dispatchEvent(new Event('resize'));
+                        }
                     }
-                });        
+                }); 
     }
 
  }
